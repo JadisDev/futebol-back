dfcdca66fa778afb8fc9ee553161588179232e48
Jadis Jale <jadis.jale@binddocs.com>
2020-12-28 14:10:12 -0300

    model game

---
+++ src/app/models/Game.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const GameSchema = new mongoose.Schema(
+  {
+    gol: {
+      type: Number,
+      required: true,
+    },
+    victory: {
+      type: Number,
+      required: true,
+    },
+    assistance: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+    },
+    groupUser: mongoose.Schema.Types.ObjectId,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Game', GameSchema);
