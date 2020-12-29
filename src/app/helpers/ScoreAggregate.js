3df9d019913833074f9f1ad04a3b3216979f2069
Jadis Jale <jadis.jale@binddocs.com>
2020-12-28 23:37:28 -0300

    ajudante com array de agregação do score

---
+++ src/app/helpers/ScoreAggregate.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+class ScoreAggregate {
+
+  async conf(collumnCount, groupId) {
+    return [
+      {
+        $lookup:
+        {
+          from: 'groupusers',
+          localField: 'groupUser',
+          foreignField: '_id',
+          as: 'groupUser'
+        }
+      },
+      { $lookup:
+        {
+          from: 'users',
+          localField: 'groupUser.user',
+          foreignField: '_id',
+          as: 'user'
+        }
+      },
+      {
+        $group: {
+          _id: {user: "$user"},
+          total: {
+            $sum: collumnCount
+          },
+          group: { $first: '$groupUser.group' }
+        }
+      },
+
+      { $match: { 'group': mongoose.Types.ObjectId(groupId) } }
+    ]
+  }
+
+}
+
+export default new ScoreAggregate();
